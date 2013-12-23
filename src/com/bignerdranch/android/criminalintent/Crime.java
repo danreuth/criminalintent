660ee4739e1b4cd2341e929891559046cd92aaa4
Dan Reuther <danreuth@gmail.com>
2013-12-22 17:14:05 -0800

    Working

--- src/com/bignerdranch/android/criminalintent/Crime.java
+++ src/com/bignerdranch/android/criminalintent/Crime.java
@@ -1,15 +1,25 @@
 package com.bignerdranch.android.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 public class Crime {
 	
 	private UUID mId;
 	private String mTitle;
+	private Date mDate;
+	private boolean mSolved;
 	
 	public Crime() {
 		// Generate a unique identifier
 		mId = UUID.randomUUID();
+		mDate = new Date();
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return mTitle;
 	}
 
 	public String getTitle() {
@@ -22,6 +32,22 @@ public class Crime {
 
 	public UUID getId() {
 		return mId;
+	}
+
+	public Date getDate() {
+		return mDate;
+	}
+
+	public void setDate(Date date) {
+		mDate = date;
+	}
+
+	public boolean isSolved() {
+		return mSolved;
+	}
+
+	public void setSolved(boolean solved) {
+		mSolved = solved;
 	}
 
 }

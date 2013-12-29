c6826bbd46dafab36e120e9d26c09425624e0568
Dan Reuther <danreuth@gmail.com>
2013-12-28 19:42:46 -0800

    Updating stuff

--- src/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ src/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -1,7 +1,13 @@
 package com.bignerdranch.android.criminalintent;
 
+import java.util.Date;
+import java.util.UUID;
+
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
@@ -15,16 +21,30 @@ import android.widget.EditText;
 
 public class CrimeFragment extends Fragment {
 
+	public static final String EXTRA_CRIME_ID = "com.bignerdranch.android.criminalintent.crime_id";
+	private static final String DIALOG_DATE = "date";
+	private static final int REQUEST_DATE = 0;
+	
 	private Crime mCrime;
 	private EditText mTitleField;
 	private Button mDateButton;
 	private CheckBox mSolvedCheckBox;	
 
+	public static CrimeFragment newInstance(UUID crimeId) {
+		Bundle args = new Bundle();
+		args.putSerializable(EXTRA_CRIME_ID, crimeId);
+		CrimeFragment fragment = new CrimeFragment();
+		fragment.setArguments(args);
+		
+		return fragment;
+	}
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		mCrime = new Crime();
+		UUID crimeId = (UUID)getArguments().getSerializable(EXTRA_CRIME_ID);
+		mCrime = CrimeLab.get(getActivity()).getCrime(crimeId);
 	}
 
 	@Override
@@ -32,6 +52,7 @@ public class CrimeFragment extends Fragment {
 			Bundle savedInstanceState) {
 		View v = inflater.inflate(R.layout.fragment_crime, container, false);
 		mTitleField = (EditText)v.findViewById(R.id.crime_title);
+		mTitleField.setText(mCrime.getTitle());
 		mTitleField.addTextChangedListener(new TextWatcher(){
 
 			@Override
@@ -55,10 +76,21 @@ public class CrimeFragment extends Fragment {
 			}});
 		
 		mDateButton = (Button)v.findViewById(R.id.crime_date);
-		mDateButton.setText(mCrime.getDate().toString());
-		mDateButton.setEnabled(false);
+		updateDate();
+		mDateButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				FragmentManager fm = getActivity().getSupportFragmentManager();
+				DatePickerFragment dialog = DatePickerFragment.newInstance(mCrime.getDate());
+				dialog.setTargetFragment(CrimeFragment.this, REQUEST_DATE);
+				dialog.show(fm, DIALOG_DATE);
+				
+			}
+		});
 		
 		mSolvedCheckBox = (CheckBox)v.findViewById(R.id.crime_solved);
+		mSolvedCheckBox.setChecked(mCrime.isSolved());
 		mSolvedCheckBox.setOnCheckedChangeListener(new OnCheckedChangeListener(){
 			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
 				mCrime.setSolved(isChecked);
@@ -66,7 +98,21 @@ public class CrimeFragment extends Fragment {
 		});
 		return v;
 	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if(resultCode != Activity.RESULT_OK){
+			return;
+		}
+		if(requestCode == REQUEST_DATE){
+			Date date = (Date)data.getSerializableExtra(DatePickerFragment.EXTRA_DATE);
+			mCrime.setDate(date);
+			updateDate();
+		}
+	}
 	
-	
+	private void updateDate() {
+		mDateButton.setText(mCrime.getDate().toString());
+	}
 	
 }
--- src/com/bignerdranch/android/criminalintent/CrimeListFragment.java
+++ src/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -2,6 +2,7 @@ package com.bignerdranch.android.criminalintent;
 
 import java.util.ArrayList;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.util.Log;
@@ -30,8 +31,20 @@ public class CrimeListFragment extends ListFragment {
 	@Override
 	public void onListItemClick(ListView l, View v, int position, long id) {
 		Crime c = ((CrimeAdapter)getListAdapter()).getItem(position);
-		Log.d(TAG, c.getTitle() + " was clicked");
+		
+		Intent i = new Intent(getActivity(), CrimePagerActivity.class);
+		i.putExtra(CrimeFragment.EXTRA_CRIME_ID, c.getId());
+		startActivity(i);
 	}
+
+	
+	@Override
+	public void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		((CrimeAdapter)getListAdapter()).notifyDataSetChanged();
+	}
+
 
 	private class CrimeAdapter extends ArrayAdapter<Crime> {
 		
